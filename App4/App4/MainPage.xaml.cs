9203bcf8fe7175a9084d268f35bf27a53cf09adf
RogelioCordova <55772023+droozyson@users.noreply.github.com>
2020-05-04 13:05:59 -0700

    timer

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        Stopwatch stopwatch;
+        //reference https://www.youtube.com/watch?v=BOx1_fsC-VM
+        public MainPage()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+
+            lblStopwatch.Text = "00:00:00.00000";
+        }
+        private void btnStart_Clicked(object sender, EventArgs e)
+        {
+            if (!stopwatch.IsRunning)
+            {
+                stopwatch.Start();
+
+                Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
+                {
+                    lblStopwatch.Text = stopwatch.Elapsed.ToString();
+
+                    if (!stopwatch.IsRunning)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+
+                }
+
+                );
+            }
+
+
+
+        }
+
+        private void btnStop_Clicked(object sender, EventArgs e)
+        {
+            btnStart.Text = "Resume";
+            stopwatch.Stop();
+        }
+
+        private void btnReset_Clicked(object sender, EventArgs e)
+        {
+            lblStopwatch.Text = "00:00:00.00000";
+            btnStart.Text = "Start";
+            stopwatch.Reset();
+        }
+    }
+}
